166dd86aa347d408d1c453530ed70d87d94c876c
Giovanni Sánchez <giovannis@avetti.ca>
2022-09-23 16:26:21 -0400

    express multiple urls

--- server.js
+++ server.js
@@ -5,11 +5,35 @@ const app = express()
 
 // add http request handler
 let hello = (req, res) => {
-   res.writeHead(200)
-   res.end('Hello World')
+    res.writeHead(200)
+    res.write('Hello World')
+    res.end()
+}
+
+let goodbye = (req, res) => {
+    res.writeHead(200)
+    res.write('Good bye World')
+    res.end()
+}
+
+let index = (req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200)
+        res.write('Home Page')
+        res.end()
+    }
+    else {
+        res.writeHead(404)
+        res.write('Not Found')
+        res.end()
+    }
 }
 //route http requests to our handler function
-app.use(hello)
+//index path must go last or it executes on all urls
+app.use('/hello', hello)
+app.use('/goodbye', goodbye)
+app.use('/', index) //also no characters works
+
 
 //start express web server
 app.listen(3000)
